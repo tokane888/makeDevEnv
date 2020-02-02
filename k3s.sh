d69e9ed472384260b00b3527f935d729be61774c
tokane888 <tokane888@gmail.com>
2020-02-02 19:46:07 +0900

    virt-host-validateによる仮想化確認手順追加

--- k3s.sh
+++ k3s.sh
@@ -11,6 +11,12 @@ check_virtualization_support() {
     echo Virtualization is not enabled on this machine.
     exit 1
   fi
+
+  if ! [ $(command -v virt-host-validate) ]; then
+    apt-get update -y
+    apt-get install -y libvirt-clients
+  fi
+  virt-host-validate
 }
 
 check_root() {
@@ -21,11 +27,10 @@ check_root() {
 }
 
 install_kubectl() {
-  apt-get update -y
   apt-get install -y apt-transport-https
   curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
   echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | tee -a /etc/apt/sources.list.d/kubernetes.list
-  apt-get update
+  apt-get update -y
   apt-get install -y kubectl
 }
 
@@ -43,11 +48,13 @@ cleanup() {
 }
 
 main() {
-  check_virtualization_support
   check_root
+  apt-get update -y
+  check_virtualization_support
 
   install_kubectl
   add_kubectl_completion
+  install_minikube
 
   cleanup
 }
