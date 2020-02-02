f904112aa227168214c9f86e408788dadcd0d7c2
tokane888 <tokane888@gmail.com>
2020-02-02 16:41:08 +0900

    rootで実行される前提なのでtodo削除

--- k3s.sh
+++ k3s.sh
@@ -23,10 +23,10 @@ check_root() {
 install_kubectl() {
   apt-get update -y
   apt-get install -y apt-transport-https
-  curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-  echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
-  sudo apt-get update
-  sudo apt-get install -y kubectl
+  curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+  echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | tee -a /etc/apt/sources.list.d/kubernetes.list
+  apt-get update
+  apt-get install -y kubectl
 }
 
 add_kubectl_completion() {
